b68f656496ed61014d3f8f9bba59f5cd2c170b14
Diego Bentes <diegopbentes@gmail.com>
2017-07-03 19:30:52 -0300

    Add language for validation label

--- src/Label.php
+++ src/Label.php
@@ -3,12 +3,25 @@ namespace UPhp\Languages;
 
 class Label{
     private static $language = [];
+    private static $validateLanguage = [];
 
-    public static function dbDefine(string $class, string $lang, Array $arr_language){
+    public static function dbDefine(string $class, string $lang, Array $arr_language)
+    {
         self::$language[$class][$lang] = $arr_language;
     }
 
-    public static function getLanguage($class, $lang){
+    public static function validateDefine(string $class, string $function, string $lang, Array $arr_language)
+    {
+        self::$validateLanguage[$class][$function][$lang] = $arr_language;
+    }
+
+    public static function getLanguage($class, $lang)
+    {
         return self::$language[$class][$lang];
+    }
+
+    public static function getValidateLanguage($class, $function, $lang, $field)
+    {
+        return self::$validateLanguage[$class][$function][$lang][$field];
     }
 }
